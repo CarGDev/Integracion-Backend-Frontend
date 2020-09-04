eaa1d3d4d2a7c137e1a6529366debe351504764e
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-09-04 11:37:41 -0500

    Integrate all login

--- src/server/server.js
+++ src/server/server.js
@@ -109,14 +109,14 @@ app.post("/auth/sign-in", async function (req, res, next) {
         const { token, ...user } = data;
 
         res.cookie("token", token, {
-          httpOnly: !config.dev,
-          secure: !config.dev
+          httpOnly: !(ENV === 'development'),
+          secure: !(ENV === 'development')
         });
 
         res.status(200).json(user);
       });
-    } catch (error) {
-      next(error);
+    } catch (err) {
+      next(err);
     }
   })(req, res, next);
 });
