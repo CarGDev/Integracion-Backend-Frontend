2408ad5e3d4f01f50b8376abe9de1757103eef6f
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-09-04 11:17:17 -0500

    Adding dependencies

--- src/server/server.js
+++ src/server/server.js
@@ -1,57 +1,60 @@
 /* eslint-disable func-names */
 /* eslint-disable global-require */
-import express from 'express'
-import dotenv from 'dotenv'
-import helmet from 'helmet'
-import webpack from 'webpack'
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import { StaticRouter } from 'react-router-dom'
-import { renderRoutes } from 'react-router-config'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducer from '../frontend/reducers'
-import Layout from '../frontend/components/Layout'
-import initialState from '../frontend/initialState'
-import serverRoutes from '../frontend/routes/serverRoutes'
-import getManifest from './getManifest'
-import cookieParser from 'cookie-parser'
-import passport from 'passport'
+import express from 'express';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import webpack from 'webpack';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from '../frontend/reducers';
+import Layout from '../frontend/components/Layout';
+import initialState from '../frontend/initialState';
+import serverRoutes from '../frontend/routes/serverRoutes';
+import getManifest from './getManifest';
 
-dotenv.config()
+import cookieParser from 'cookie-parser';
+import boom from '@hapi/boom';
+import passport from 'passport';
+import axios from 'axios';
 
-const app = express()
-const { ENV, PORT } = process.env
+dotenv.config();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(passport.initialize())
-app.use(passport.session())
+const app = express();
+const { ENV, PORT } = process.env;
 
-require('./utils/auth/strategies/basic')
+app.use(express.json());
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./utils/auth/strategies/basic');
 
 if (ENV === 'development') {
-  const webPackConfig = require('../../webpack.config')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
-  const compiler = webpack(webPackConfig)
-  const serverConfig = { port: PORT, hot: true }
-  app.use(webpackDevMiddleware(compiler, serverConfig))
-  app.use(webpackHotMiddleware(compiler))
+  const webPackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webPackConfig);
+  const serverConfig = { port: PORT, hot: true };
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
 } else {
   app.use((req, res, next) => {
-    req.hashManifest = getManifest()
-    next()
-  })
-  app.use(helmet())
-  app.use(helmet.permittedCrossDomainPolicies())
-  app.disable('x-powered-by')
+    req.hashManifest = getManifest();
+    next();
+  });
+  app.use(helmet());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.disable('x-powered-by');
 }
 
 const setResponse = (html, preloadedState, manifest) => {
-  const mainStyles = manifest ? manifest['main.css'] : '/assets/app.css'
-  const mainBuild = manifest ? manifest['main.js'] : '/assets/app.js'
-  const vendorBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js'
+  const mainStyles = manifest ? manifest['main.css'] : '/assets/app.css';
+  const mainBuild = manifest ? manifest['main.js'] : '/assets/app.js';
+  const vendorBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
   return (
     `
       <!DOCTYPE html>
@@ -73,12 +76,12 @@ const setResponse = (html, preloadedState, manifest) => {
           <script src="${vendorBuild}" type="text/javascript"></script>
         </body>
       </html>`
-  )
-}
+  );
+};
 
 const renderApp = (req, res) => {
-  const store = createStore(reducer, initialState)
-  const preloadedState = store.getState()
+  const store = createStore(reducer, initialState);
+  const preloadedState = store.getState();
   const html = renderToString(
     <Provider store={store}>
       <StaticRouter location={req.url} context={{}}>
@@ -88,12 +91,63 @@ const renderApp = (req, res) => {
       </StaticRouter>
     </Provider>
   )
-  res.send(setResponse(html, preloadedState, req.hashManifest))
-}
+  res.send(setResponse(html, preloadedState, req.hashManifest));
+};
 
-app.get('*', renderApp)
+app.post("/auth/sign-in", async function (req, res, next) {
+  passport.authenticate("basic", function (error, data) {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+
+      req.login(data, { session: false }, async function (error) {
+        if (error) {
+          next(error);
+        }
+
+        const { token, ...user } = data;
+
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post("/auth/sign-up", async function (req, res, next) {
+  const { body: user } = req;
+
+  try {
+    const userData = await axios({
+      url: `${precess.env.API_URL}/api/auth/sign-up`,
+      method: "post",
+      data: {
+        'email': user.email,
+        'name': user.name,
+        'password': user.password
+      }
+    });
+    res.status(201).json({
+      name: req.body.name,
+      email: req.body.email,
+      id: userData.data.id
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
-  if (err) console.log(err)
-  else console.log(`${ENV} server running on Port ${PORT}`)
-})
+  if (err) console.log(err);
+  else console.log(`${ENV} server running on Port ${PORT}`);
+});
